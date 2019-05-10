2cd86c6dafb1407278d8fae919af276acdae97c3
Jessica Moore <jessica.cmoore1@gmail.com>
2019-05-10 14:26:21 -0400

    Created instance method of do_battle and restart

--- player.py
+++ player.py
@@ -15,6 +15,19 @@ class Player:
     if self.gold_coins % 10 == 0:
       self.level_up()
 
+  def do_battle(self, damage):
+    self.health_points -= damage
+    if self.health_points < 1:
+      self.lives -= 1
+      self.health_points = 10
+    if self.lives < 1:
+      self.restart()
+
+  def restart(self): 
+    self.gold_coins = 0
+    self.health_points = 10
+    self.lives = 5
+
 player1 = Player()
 
 print(player1)
@@ -25,7 +38,7 @@ player1.level_up()
 print(player1)
 
 print()
-print("Collect coins!")
+print("Collect treasure!")
 player1.collect_treasure()
 player1.collect_treasure()
 player1.collect_treasure()
@@ -37,4 +50,19 @@ player1.collect_treasure()
 player1.collect_treasure()
 player1.collect_treasure()
 
+print(player1)
+
+print()
+print("Go to battle! ... Oh no, you've taken 11 damage!")
+player1.do_battle(11)
+print(player1)
+
+player1.do_battle(11)
+player1.do_battle(11)
+player1.do_battle(11)
+player1.do_battle(11)
+player1.do_battle(11)
+player1.do_battle(11)
+
+print("Wow, you're really bad at this game. -1 life")
 print(player1)
